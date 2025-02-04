e0402581b264bb3bbe62f75d6e743b914400b665
stevOOOOOOOOOO0 <stevOOOOOOOOOO0@yahoo.com>
2025-02-04 12:30:30 -0700

    words now wrap if they are too long

--- src/components/WordCard.tsx
+++ src/components/WordCard.tsx
@@ -4,12 +4,21 @@ import "../App.css";
 
 function WordCard() {
   const word = generateSlug(1, { partsOfSpeech: ["noun"] });
+  const textSize = window.innerWidth * 0.0355;
+  const textWidth = window.innerWidth / 6;
 
   return (
     <div className="imageWrapper">
       <img src={imageToAdd} alt="..." width={window.innerWidth / 5} />
-      <p className="cardText">{word}</p>
-      <p className="flippedCardText">{word}</p>
+      <p className="cardText" style={{ fontSize: textSize, width: textWidth }}>
+        {word}
+      </p>
+      <p
+        className="flippedCardText"
+        style={{ fontSize: textSize, width: textWidth }}
+      >
+        {word}
+      </p>
     </div>
   );
 }
