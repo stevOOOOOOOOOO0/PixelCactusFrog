a6d59603d658220683df6f4b8f0ed1cbeaa31b1d
stevOOOOOOOOOO0 <stevOOOOOOOOOO0@yahoo.com>
2025-02-04 20:54:48 -0700

    again

--- src/components/WordCard.tsx
+++ src/components/WordCard.tsx
@@ -1,20 +1,13 @@
 import { generateSlug } from "random-word-slugs";
-//import { useState } from "react";
 import imageToAdd from "../assets/Card-Background.svg";
 import "../App.css";
 
 function WordCard() {
   const word = generateSlug(1, { partsOfSpeech: ["noun"] });
-  //const [word, setWords] = useState("START");
 
   return (
     <div className="imageWrapper">
-      <img
-        className="imageDiv"
-        src={imageToAdd}
-        alt="..."
-        //onClick={() => setWords((word) => generateSlug(1))}
-      />
+      <img className="imageDiv" src={imageToAdd} alt="..." />
       <p className="cardText">{word}</p>
       <p className="flippedCardText">{word}</p>
     </div>
