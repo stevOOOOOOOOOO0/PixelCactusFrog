d3e2e120550b263639f9dfe97448d8c467f9d3a5
stevOOOOOOOOOO0 <stevOOOOOOOOOO0@yahoo.com>
2025-02-06 18:23:48 -0700

    buttons in correct positions

--- src/App.tsx
+++ src/App.tsx
@@ -12,44 +12,45 @@ function App() {
 
   return (
     <>
-      <div className="buttonContainer flippedOrientationButtons">
-        <button
-          onClick={() => setCount3((count3) => count3 + 1)}
-          className="playerButton"
+      <div className="flexContainer">
+        <div className="buttonContainer flippedOrientationButtons">
+          <button
+            onClick={() => setCount3((count3) => count3 + 1)}
+            className="playerButton"
+          >
+            <text className="buttonText">Player 3: {count3} </text>
+          </button>
+          <button
+            onClick={() => setCount4((count4) => count4 + 1)}
+            className="playerButton"
+          >
+            <text className="buttonText">Player 4: {count4} </text>
+          </button>
+        </div>
+        <div
+          className="cardContainer"
+          onClick={() => setKey((prevKey) => prevKey + 1)}
         >
-          <text className="buttonText">Player 3: {count3} </text>
-        </button>
-        <button
-          onClick={() => setCount4((count4) => count4 + 1)}
-          className="playerButton"
-        >
-          <text className="buttonText">Player 4: {count4} </text>
-        </button>
-      </div>
-      <div
-        className="cardContainer"
-        onClick={() => setKey((prevKey) => prevKey + 1)}
-      >
-        <WordCard />
-        <WordCard />
-        <WordCard />
-      </div>
-      <div className="buttonContainer">
-        <button
-          onClick={() => setCount1((count1) => count1 + 1)}
-          className="playerButton"
-        >
-          <text className="buttonText">Player 1: {count1} </text>
-        </button>
-        <button
-          onClick={() => setCount2((count2) => count2 + 1)}
-          className="playerButton"
-        >
-          <text className="buttonText">Player 2: {count2} </text>
-        </button>
+          <WordCard />
+          <WordCard />
+          <WordCard />
+        </div>
+        <div className="buttonContainer">
+          <button
+            onClick={() => setCount1((count1) => count1 + 1)}
+            className="playerButton"
+          >
+            <text className="buttonText">Player 1: {count1} </text>
+          </button>
+          <button
+            onClick={() => setCount2((count2) => count2 + 1)}
+            className="playerButton"
+          >
+            <text className="buttonText">Player 2: {count2} </text>
+          </button>
+        </div>
       </div>
       <HamburgerMenu></HamburgerMenu>
-      {key && <div></div>}
     </>
   );
 }
