a86328a0685f3bfd277644732c4b6eba9bfac49e
stevOOOOOOOOOO0 <stevOOOOOOOOOO0@yahoo.com>
2025-02-08 00:41:35 -0700

    remaking the cards into seperate pieces

--- src/components/WordCard.tsx
+++ src/components/WordCard.tsx
@@ -6,6 +6,15 @@ function WordCard() {
   const word = generateSlug(1, { partsOfSpeech: ["noun"] });
 
   return (
+    <>
+      <div className="imageWrapper">
+        <div className="blankCard"></div>
+        <p className="cardText">{word}</p>
+      </div>
+    </>
+  );
+
+  return (
     <div className="imageWrapper">
       <img className="imageDiv" src={imageToAdd} alt="..." />
       <p className="cardText">{word}</p>
